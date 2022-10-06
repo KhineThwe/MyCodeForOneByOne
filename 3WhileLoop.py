cc81639204b72fdec78f0c1386537cba2710d11e
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-06 18:50:40 +0630

    Create 3WhileLoop.py

---
+++ 3WhileLoop.py
@@ -0,0 +1,25 @@
+# While loop
+# ----------
+# #code block တစ်ခုကိုကျော်သွားချင်တဲ့အခါမျိုး code block တစ်ခုအတွင်းရှိ instruction
+# များကိုလုပ်ဆောင်ချင်တဲ့အခါမျိုးတွင်သုံးသည်။
+# #ထပ်ခါထပ်ခါလုပ်ဆောင်ချင်လိုသောအခါမျိုးတွင်သုံးသည်။
+#while condition: if True
+    # Instruction of while loop Do this
+# while a < 5:if True
+#    print("Hello",a) Do this
+#    a++
+#python မှာဆိုရင် Block တစ်ခုနဲ့ တစ်ခုကို space indent တွေနဲ့ခွဲထားတယ်
+# def myFun1():
+#     print("This is my fun1")
+# print("This is testing")
+
+# def myFun2():
+#     print("This is my fun2")
+a=20
+b=20
+#while code block start
+while a<b:
+    print("{} Hello i am khine".format(a))
+    a+=1
+#while code block end
+print("Nothing do!")
