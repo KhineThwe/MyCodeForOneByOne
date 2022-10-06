3a5d3dc1391e3537991ff9a343f6e241ce08ccb6
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-06 18:53:35 +0630

    Create 10StringLesson.py

---
+++ 10StringLesson.py
@@ -0,0 +1,32 @@
+#" " or ' ' multiline ll ya
+
+print("""
+My name
+
+is 
+
+
+Khine
+
+
+""".upper())
+
+#isalpha() to check if a string contains only characters and is not empty
+#isalnum() to check if a string contains characters or digits and is not empty
+#isdecimal() to check if a string contains digits and is not empty
+#.lower() or .title()
+#islower()
+#isupper()
+#startsswith to check if the string starts with a specific substring
+#endswith() to check if the string ends with a specific substring 
+#replace() to replace a part of a string 
+#split() to split a string on a specific character separator
+
+#strip() to trim the whitespacce from a string
+#join() to append new letters to a string
+#find() to find the position of a substring
+
+name = "Khine"
+print(name.lower())
+print("au" in name)
+print(ord('A'))
