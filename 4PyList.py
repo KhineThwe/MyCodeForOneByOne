3060af6b44a46b3a170592511db7a6ea4d33fa33
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-18 06:41:50 +0630

    Create 4PyList.py

---
+++ 4PyList.py
@@ -0,0 +1,11 @@
+subList = ['myanmar','math','physic','cp']
+subList[-1] = 'algorithm'
+rm = 'physics'
+print(subList)
+subList.pop(3)
+print('After using pop method\n')
+print(subList)
+print('After using del\n')
+#del ka key pl
+del subList[0]
+print(subList)
