1a28b1e6e68f9ffb55c77d4691a2f7ae8b6de97d
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-18 06:43:08 +0630

    Create 6Loopwithlist.py

---
+++ 6Loopwithlist.py
@@ -0,0 +1,14 @@
+fruitList = ['apple','banana','orange','watermelon']
+numberList = [1,2,3,4,5,88,99]
+
+
+for element in fruitList:
+    if(element == 'apple'):
+        print(element)
+        # break
+    else:
+        print('not found zoo')
+
+for e in numberList:
+    e+=3
+    print(e)
