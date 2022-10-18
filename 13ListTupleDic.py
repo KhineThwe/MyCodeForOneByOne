74b1ae457b7cd2ff992b7bd2def1b67e0ceb3723
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-18 06:45:12 +0630

    Create 13ListTupleDic.py

---
+++ 13ListTupleDic.py
@@ -0,0 +1,17 @@
+#list
+#tuple
+#dictionary
+#set
+
+list = [1,2,3,4,5]#order #index use piထုတ်နိုင် 
+tu = (1,2,3,4,5)#order
+se = {1,2,3,4,5}#unorder 
+dic = {'a':10,'b':20,'c':30}#unorder
+print(list[1])
+print(tu[2])#access ပြုလုပ်ရင်တော့ဒီပုံပဲ
+# print(se[4])#error tat mal index nae ma ထုတ်နိုင် 
+print(se)
+#print(dic[1]) #index nae ma ya
+
+#list tuple ရဲ့ memory address တွေက အစဉ်လိုက်လေး ဖြစ်နေပေမဲ့
+#set dic ရဲ့ memory address တွေရဲ့ address တွေကတော့ အစဉ်လိုက်မဟုတ်
