40862de9ea0301d2acf8fdaa4050d75e0ca51e68
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-11-03 12:35:48 -1200

    Create bubbleSort.py

---
+++ bubbleSort.py
@@ -0,0 +1,17 @@
+#https://technologystrive.com/bubble-sort/
+# Bubble Sort in python
+def bubbleSort(array):
+    for i in range(len(array)):
+        for j in range(0,len(array)-i-1):#
+            #compare two adjacent elements
+            #change > to < to sort in descending order
+            if array[j] > array[j+1]:
+                #swapping elements if elements
+                #are not in the intended order
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+data = [-2,45,0,11,-9]
+bubbleSort(data)
+print('Sorted Array in Ascending Order: ')
+print(data)
