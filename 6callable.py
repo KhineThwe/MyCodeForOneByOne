ff10d5e4b61725ccee61b7cc4e2c1158f40a3dee
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-11-14 22:13:48 -1200

    Create 6callable.py

---
+++ 6callable.py
@@ -0,0 +1,18 @@
+#class တစ်ခုတည်းမှာ method တစ်ခုခုဆိုရင် callable ဖြစ်သင့်ပေမဲ့
+class myClass:
+    def __init__(self,x=0):#x=default parameter
+        print('from class')
+        self.counter = x
+
+    def __call__(self,x=1):
+        print('updating value.............')
+        self.counter +=x
+
+obj = myClass()
+inObj = myClass()
+print(myClass.__init__(inObj,1))#className.methodName(obj name,value ka optional)
+print(inObj.counter)
+print(myClass.__call__(obj,10))#callable pyhit ag lote tr
+print(obj.counter)
+print(callable(obj))
+#how to make callable
