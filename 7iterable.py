945b9bc644f5b2cff82da80b1311cc34c51d7859
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-11-14 22:22:05 -1200

    Create 7iterable.py

---
+++ 7iterable.py
@@ -0,0 +1,11 @@
+#iterable
+#db nae ပတ်သတ်လို့ pj ရေးရင် အသုံးဝင်တဲ့ကောင်တွေဖြစ်တယ်
+list1 = ['a','b','c','d','e']
+iterator = iter(list1)
+#iter fun ka iterator object ကို return ပြန်ပေးတယ် သူတို့က တစ်ကြိမ်မှာ တစ်ခုပဲ return ပြန်ပေးတယ် သူ့ထဲမှာရှိတဲ့ next item next item ကို ပြန်ပေးခြင်းဖြစ်တယ်။ 
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(iterator)
+print(list1)
