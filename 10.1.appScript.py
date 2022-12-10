a45d148967251b55cfbc6f6ba5e55f12af12b146
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-12-09 20:03:47 -1200

    Create 10.1.appScript.py

---
+++ 10.1.appScript.py
@@ -0,0 +1,11 @@
+# from myModule import fibo,listFibo
+#
+# fibo(10)
+# print('\nFibo in list: ')
+# print(listFibo(100))
+
+#another way
+import myModule
+
+myModule.fibo(5)
+print(myModule.listFibo(100))
