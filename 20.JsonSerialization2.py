8524eef1fdab299bcb26b746dfbc570851397c8f
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-12-14 16:08:47 -1200

    Create 20.JsonSerialization2.py

---
+++ 20.JsonSerialization2.py
@@ -0,0 +1,18 @@
+# https://docs.python.org/3/library/json.html
+#https://jsonlint.com/
+#to check valid json
+import json
+myJson = {
+ "name" : "khine",
+ "age": 25,
+ "hobby":["coding","training","building iot"]
+}#python obj
+#convert python obj to json format
+
+#dumps vs dump
+#serialization လုပ်တယ်ဆိုတာက python type တွေကနေ json format အဖြစ်သို့ သွား
+# ချိန်းတာကိုဆိုလိုခြင်းဖြစ်တယ်
+
+with open("jdataFile.json", "w") as jsFile:
+    json.dump(myJson,jsFile,indent=2,sort_keys=True)
+#dump 2 parameter --- > python obj and json file
